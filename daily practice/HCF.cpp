43d07d2f0982e8604158bd4b104669be0aa7965b
Adamyaaa <arcticlunitic@gmail.com>
2025-08-09 21:56:51 +0530

    not optimal

--- daily practice/HCF.cpp
+++ daily practice/HCF.cpp
@@ -1,6 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int HCForGCD(int n1, int n2){
+int HCForGCD(int n1,int n2){
+    int gcd =0;
+    if(n1==0 || n2==0){
+        return max(n1,n2);
+    }
+    else{
+    for ( int i = 1; i <= min(n1,n2); i++)
+    {
+
+        if (n1%i==0 && n2%i==0){
+            gcd=i;
+            
+        }
+
+    }
+    return gcd;
+}
+}
+int main(){
+    int n1,n2;
+    cin>>n1>>n2;
+  cout<< HCForGCD(n1,n2);
     
+    
+  
 }
